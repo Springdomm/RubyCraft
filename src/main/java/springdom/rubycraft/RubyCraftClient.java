93db88f1a0febac45d4e62494140f36970411047
UmbraeFR <anderson_mason@icloud.com>
2024-12-07 22:26:30 -0500

    Stuff

---
+++ src/main/java/springdom/rubycraft/RubyCraftClient.java
@@ -0,0 +1,10 @@
+package springdom.rubycraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
